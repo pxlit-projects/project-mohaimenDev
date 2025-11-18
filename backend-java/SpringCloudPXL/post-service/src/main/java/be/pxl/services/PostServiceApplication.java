d6c1664e40239fa8a21d0de6702dbef71196dc1a
mohaimenpxl <Mohaimen.AlJanabi@STUDENT.PXL.BE>
2025-11-18 20:42:23 +0100

    post-service opzetten.

---
+++ backend-java/SpringCloudPXL/post-service/src/main/java/be/pxl/services/PostServiceApplication.java
@@ -0,0 +1,18 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EmployeeServiceApplication
+ *
+ */
+
+@SpringBootApplication
+public class PostServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(PostServiceApplication.class, args);
+    }
+}
---
+++ backend-java/SpringCloudPXL/src/test/java/be/pxl/services/SpringCloudPxlApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudPxlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
